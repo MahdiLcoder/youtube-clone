733fffd47b3663de168955bfa9c9b0da6d56c3da
MahdiLcoder <labidimahdi95@gmail.com>
2024-09-14 14:16:43 +0100

    complete youtube clone

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Recommended.css";
 import thumbnail1 from "../../assets/thumbnail1.png";
 import thumbnail2 from "../../assets/thumbnail2.png";
@@ -8,74 +8,34 @@ import thumbnail5 from "../../assets/thumbnail5.png";
 import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
-function Recommended(){
-
+import { useState } from "react";
+import { API_KEY, valueConverter } from "../../data";
+import { Link } from "react-router-dom";
+function Recommended({categoryId}){
+	const [apiData, setApiData] = useState([])
+	const fetchVideoData = async () => {
+		const videoData = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+		const res = await fetch(videoData);
+		const data = await res.json();
+		setApiData(data.items);
+	}
+	useEffect(() => {
+		fetchVideoData();
+	},[])
 	return(
 		<div className="recommended">
-			<div className="side-video-list">
-				<img src={thumbnail1} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail2} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail3} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail4} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail5} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail6} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail7} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
-			<div className="side-video-list">
-				<img src={thumbnail8} alt="" />
-				<div className="info">
-					<h4>Best channel that help you to be a web developer</h4>
-					<p>GreatStack</p>
-					<p>199k Views</p>
-				</div>
-			</div>
+			{apiData.map((item,i)=>{
+				return(
+						<Link to={`/video/${item.snippet.categoryId}/${item.id}`}className="side-video-list" key={i}>
+							<img src={item.snippet.thumbnails.medium.url} alt="" />
+							<div className="info">
+								<h4>{item.snippet.title}</h4>
+								<p>{item.snippet.channelTitle}</p>
+								<p>{valueConverter(item.statistics.viewCount)}Views</p>
+							</div>
+					</Link>
+				)
+			})}
 			
 		</div>
 	)
